4dfeb3c82642700a35762fcd805db02e37e222dc
Sefa Ilkimen <sefa.ilkimen@mobisys.de>
2017-10-02 12:33:30 +0200

    - using plugin v1.6.0 - added PATCH button

--- www/js/index.js
+++ www/js/index.js
@@ -5,6 +5,8 @@ const app = {
     initialize: function() {
         document.getElementById('toggleAllCertsBtn').addEventListener('click', this.onToggleAllCertsBtnClick.bind(this));
         document.getElementById('getBtn').addEventListener('click', this.onGetBtnClick.bind(this));
+        document.getElementById('patchBtn').addEventListener('click', this.onPatchBtnClick.bind(this));
+        // document.getElementById('urlInput').value = 'http://172.16.29.50:8085/test';
     },
 
     print: function(content) {
@@ -15,6 +17,12 @@ const app = {
       const url = document.getElementById('urlInput').value;
 
       cordova.plugin.http.get(url, {}, {}, data => this.print(data), error => this.print(error));
+    },
+
+    onPatchBtnClick: function() {
+      const url = document.getElementById('urlInput').value;
+
+      cordova.plugin.http.patch(url, { test: 'testString' }, {}, data => this.print(data), error => this.print(error));
     },
 
     onToggleAllCertsBtnClick: function() {
